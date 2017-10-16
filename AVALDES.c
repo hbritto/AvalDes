a774c5c22f2e3a916ca4d04ddcad93d533df097e
Henrique Bonini de Britto Menezes <henrique.b.britto@gmail.com>
2017-10-16 14:56:28 -0200

    Corrections

--- AVALDES.c
+++ AVALDES.c
@@ -1,6 +1,12 @@
 /* ----------------------------------------------------------------------- 
 C�digo gerado com o ASDA - Ambiente de Simula��o Distribu�da Autom�tico
 --------------------------------------------------------------------------*/
+#ifndef AVALDES
+#define AVALDES
+
+// #define MAX_CLIENTS 100
+
+#include <stdlib.h>
 #include <stdio.h>
 #include "smpl.h"
 
@@ -15,20 +21,30 @@ typedef struct request_t {
 	float ts_end;
 } request_t;
 
-int main() {
+int main(int argc, char *argv[]) {
 	/* definicoes */
 	float Te = 1;
-	request_t client[100];
+	unsigned int curr_clients = 0, max_clients = 100;
+	request_t client[max_clients];
 	int event, next_client = 1, aleatorio;
-	int disk_service_time[] = {10, 50};
-	int CPU_service_time[] = {1, 5};
-	float Ta0 = 10, Ts0 = 10, Ts1 = 10, Ts2 = 10, Ts3 = 10, Ts4 = 10, Ts5 = 10, Ts6 = 10, Ts7 = 10, Ts8 = 10;
+	double arrival_time = expntl(10);
+	double disk_service_time[] = {expntl(10), expntl(15)};
+	double CPU_service_time[] = {expntl(20), expntl(30)};
+	// float Ta0 = 10, Ts0 = 10, Ts1 = 10, Ts2 = 10, Ts3 = 10, Ts4 = 10, Ts5 = 10, Ts6 = 10, Ts7 = 10, Ts8 = 10;
 	int i = 0;
 	request_t *c;
 
-	unsigned int curr_clients = 0, max_clients = 100;
+	// int bugfix = 100;
+	// request_t *client = (request_t *) malloc(sizeof(request_t) * max_clients);
+
+	/* escalona a chegada do primeiro cliente */
+	int seed = atoi(argv[1]);
+	stream(seed);
+
 	FILE *p, *saida;
-	saida = fopen("AVALDES.out", "w");
+	char filename[100];
+	sprintf(filename, "AVALDES_%s.out", argv[1]);
+	saida = fopen(filename, "w");
 
 	if ((p = sendto(saida)) == NULL)
 		printf("Erro na saida \n");
@@ -47,11 +63,8 @@ int main() {
 	facility("CPU4", 1);
 	facility("Disk4", 1);
 
-	/* escalona a chegada do primeiro cliente */
-	stream(0);
-
-	schedule(1, 1.0, 0);
-	while ((time() < Te) && (curr_clients < max_clients)) {
+	schedule(1, 0.0, 0);
+	while (curr_clients < max_clients) {
 		cause(&event, &i);
 		c = &client[i];
 
@@ -65,13 +78,13 @@ int main() {
 		case 1:
 			schedule(2, 0.0, i);
 
-			schedule(1, expntl(Ta0), next_client++);
+			schedule(1, arrival_time, next_client++);
 			break;
 
 		/*  centro de serviço = Front */
 		case 2:
 			if (request("Front", event, i, 0) == 0)
-				schedule(3, expntl(Ts0), i);
+				schedule(3, 0.0, i);
 			break;
 		case 3:
 			release("Front", i);
@@ -90,7 +103,7 @@ int main() {
 		/*  centro de serviço = CPU1 */
 		case 4:
 			if (request("CPU1", event, i, 0) == 0)
-				schedule(5, expntl(Ts1), i);
+				schedule(5, CPU_service_time[c->type], i);
 			break;
 		case 5:
 			release("CPU1", i);
@@ -105,7 +118,7 @@ int main() {
 		/*  centro de serviço = CPU2 */
 		case 8:
 			if (request("CPU2", event, i, 0) == 0)
-				schedule(9, expntl(Ts3), i);
+				schedule(9, CPU_service_time[c->type], i);
 			break;
 		case 9:
 			release("CPU2", i);
@@ -120,7 +133,7 @@ int main() {
 		/*  centro de serviço = CPU3 */
 		case 12:
 			if (request("CPU3", event, i, 0) == 0)
-				schedule(13, expntl(Ts5), i);
+				schedule(13, CPU_service_time[c->type], i);
 			break;
 		case 13:
 			release("CPU3", i);
@@ -135,7 +148,7 @@ int main() {
 		/*  centro de serviço = CPU4 */
 		case 16:
 			if (request("CPU4", event, i, 0) == 0)
-				schedule(17, expntl(Ts7), i);
+				schedule(17, CPU_service_time[c->type], i);
 			break;
 		case 17:
 			release("CPU4", i);
@@ -150,7 +163,7 @@ int main() {
 		/*  centro de serviço = Disk1 */
 		case 6:
 			if (request("Disk1", event, i, 0) == 0)
-				schedule(7, expntl(Ts2), i);
+				schedule(7, disk_service_time[c->type], i);
 			break;
 		case 7:
 			release("Disk1", i);
@@ -160,7 +173,7 @@ int main() {
 		/*  centro de serviço = Disk2 */
 		case 10:
 			if (request("Disk2", event, i, 0) == 0)
-				schedule(11, expntl(Ts4), i);
+				schedule(11, disk_service_time[c->type], i);
 			break;
 		case 11:
 			release("Disk2", i);
@@ -170,7 +183,7 @@ int main() {
 		/*  centro de serviço = Disk3 */
 		case 14:
 			if (request("Disk3", event, i, 0) == 0)
-				schedule(15, expntl(Ts6), i);
+				schedule(15, disk_service_time[c->type], i);
 			break;
 		case 15:
 			release("Disk3", i);
@@ -180,7 +193,7 @@ int main() {
 		/*  centro de serviço = Disk4 */
 		case 18:
 			if (request("Disk4", event, i, 0) == 0)
-				schedule(19, expntl(Ts8), i);
+				schedule(19, disk_service_time[c->type], i);
 			break;
 		case 19:
 			release("Disk4", i);
@@ -189,33 +202,35 @@ int main() {
 		}
 	}
 
+	// free(client);
+
 	/* gera o relatorio da simulacao */
 	fprintf(saida, "TempoSimulado: %f\n", time());
 
 	fprintf(saida, "Utilizacao (\"Front\") = %g\n", utilizacao_recurso("Front"));
 	fprintf(saida, "Comprimento medio fila (\"Front\") = %g\n", comprimento_medio_fila("Front"));
-	fprintf(saida, "Periodo medio ocupado (\"Front\") = %g\n", periodo_medio_ocupado("Front"));
+	fprintf(saida, "Periodo medio ocupado (\"Front\") = %g\n\n", periodo_medio_ocupado("Front"));
 	fprintf(saida, "Utilizacao (\"CPU1\") = %g\n", utilizacao_recurso("CPU1"));
 	fprintf(saida, "Comprimento medio fila (\"CPU1\") = %g\n", comprimento_medio_fila("CPU1"));
-	fprintf(saida, "Periodo medio ocupado (\"CPU1\") = %g\n", periodo_medio_ocupado("CPU1"));
+	fprintf(saida, "Periodo medio ocupado (\"CPU1\") = %g\n\n", periodo_medio_ocupado("CPU1"));
 	fprintf(saida, "Utilizacao (\"Disk1\") = %g\n", utilizacao_recurso("Disk1"));
 	fprintf(saida, "Comprimento medio fila (\"Disk1\") = %g\n", comprimento_medio_fila("Disk1"));
-	fprintf(saida, "Periodo medio ocupado (\"Disk1\") = %g\n", periodo_medio_ocupado("Disk1"));
+	fprintf(saida, "Periodo medio ocupado (\"Disk1\") = %g\n\n", periodo_medio_ocupado("Disk1"));
 	fprintf(saida, "Utilizacao (\"CPU2\") = %g\n", utilizacao_recurso("CPU2"));
 	fprintf(saida, "Comprimento medio fila (\"CPU2\") = %g\n", comprimento_medio_fila("CPU2"));
-	fprintf(saida, "Periodo medio ocupado (\"CPU2\") = %g\n", periodo_medio_ocupado("CPU2"));
+	fprintf(saida, "Periodo medio ocupado (\"CPU2\") = %g\n\n", periodo_medio_ocupado("CPU2"));
 	fprintf(saida, "Utilizacao (\"Disk2\") = %g\n", utilizacao_recurso("Disk2"));
 	fprintf(saida, "Comprimento medio fila (\"Disk2\") = %g\n", comprimento_medio_fila("Disk2"));
-	fprintf(saida, "Periodo medio ocupado (\"Disk2\") = %g\n", periodo_medio_ocupado("Disk2"));
+	fprintf(saida, "Periodo medio ocupado (\"Disk2\") = %g\n\n", periodo_medio_ocupado("Disk2"));
 	fprintf(saida, "Utilizacao (\"CPU3\") = %g\n", utilizacao_recurso("CPU3"));
 	fprintf(saida, "Comprimento medio fila (\"CPU3\") = %g\n", comprimento_medio_fila("CPU3"));
-	fprintf(saida, "Periodo medio ocupado (\"CPU3\") = %g\n", periodo_medio_ocupado("CPU3"));
+	fprintf(saida, "Periodo medio ocupado (\"CPU3\") = %g\n\n", periodo_medio_ocupado("CPU3"));
 	fprintf(saida, "Utilizacao (\"Disk3\") = %g\n", utilizacao_recurso("Disk3"));
 	fprintf(saida, "Comprimento medio fila (\"Disk3\") = %g\n", comprimento_medio_fila("Disk3"));
-	fprintf(saida, "Periodo medio ocupado (\"Disk3\") = %g\n", periodo_medio_ocupado("Disk3"));
+	fprintf(saida, "Periodo medio ocupado (\"Disk3\") = %g\n\n", periodo_medio_ocupado("Disk3"));
 	fprintf(saida, "Utilizacao (\"CPU4\") = %g\n", utilizacao_recurso("CPU4"));
 	fprintf(saida, "Comprimento medio fila (\"CPU4\") = %g\n", comprimento_medio_fila("CPU4"));
-	fprintf(saida, "Periodo medio ocupado (\"CPU4\") = %g\n", periodo_medio_ocupado("CPU4"));
+	fprintf(saida, "Periodo medio ocupado (\"CPU4\") = %g\n\n", periodo_medio_ocupado("CPU4"));
 	fprintf(saida, "Utilizacao (\"Disk4\") = %g\n", utilizacao_recurso("Disk4"));
 	fprintf(saida, "Comprimento medio fila (\"Disk4\") = %g\n", comprimento_medio_fila("Disk4"));
 	fprintf(saida, "Periodo medio ocupado (\"Disk4\") = %g\n", periodo_medio_ocupado("Disk4"));
@@ -224,4 +239,6 @@ int main() {
 
 	return 0;
 }
+
+#endif
 /* ----------------------------------------------------------------------- */
